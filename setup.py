5d895f07fed7c9ce1ae65bde62ff0c07537cc865
James Wootton <decodoku@gmail.com>
2020-06-16 19:21:51 +0200

    try again with different syntax

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setup(name='quantumgraph',
-      install_requires=requirements,
+      install_requires=['qiskit', 'scipy', 'pairwise_tomography'],
       version='0.0.1',
       packages=['quantumgraph']
 )
