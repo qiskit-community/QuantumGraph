90dbb78d7e051417f88cda065a3ef714061c521d
jwo <jwo@zurich.ibm.com>
2020-06-16 16:42:54 +0200

    use requirements.txt for requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(name='quantumgraph',
-      install_requires=['qiskit', 'scipy', 'pairwise_tomography'],
+      install_requires=requirements,
       version='0.0.1',
       packages=[package for package in find_packages()
                 if package.startswith('quantumgraph')]
