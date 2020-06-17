45ea3eeb845e34b9be36e9b40b03aadc74061825
jwo <jwo@zurich.ibm.com>
2020-06-17 10:32:16 +0200

    remove capitals from module name

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(name='quantumgraph',
       install_requires=['qiskit', 'scipy', 'pairwise_tomography'],
       version='0.0.1',
-      packages=['QuantumGraph']
+      packages=['quantumgraph']
 )
