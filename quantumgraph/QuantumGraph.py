908efaca28d0ce7d7df291d376f0a95525e3177d
Owais Ishtiaq Siddiqui <owaisishtiaq15@gmail.com>
2025-06-02 17:54:52 +0200

    Updated Quantum-Graph to Qiskit 2.0
    Removed Qiskit-Ignis dependency and update the QuantumGraph to Qiskit 2.0.
    
    A running tutorial file.
    All test files are running.

--- quantumgraph/QuantumGraph.py
+++ quantumgraph/QuantumGraph.py
@@ -1,11 +1,30 @@
-from qiskit import QuantumCircuit, execute, Aer
-from qiskit.quantum_info.synthesis import OneQubitEulerDecomposer
-from qiskit.quantum_info.synthesis.two_qubit_decompose import two_qubit_cnot_decompose
+#from qiskit import QuantumCircuit, execute, Aer #(not supported in qiskit 2.x.x)
+from qiskit import transpile, QuantumCircuit #(corrected to qiskit 2.0.2 version)
+from qiskit_aer import AerSimulator #(corrected to qiskit-aer version 0.17.x compatible with qiskit 2.0.2)
 
-from pairwise_tomography.pairwise_state_tomography_circuits import pairwise_state_tomography_circuits
-from pairwise_tomography.pairwise_fitter import PairwiseStateTomographyFitter
 
-from .ExpectationValue import ExpectationValue
+'''
+previously written
+# from qiskit.quantum_info.synthesis import OneQubitEulerDecomposer
+# from qiskit.quantum_info.synthesis.two_qubit_decompose import two_qubit_cnot_decompose
+'''
+
+# corrected to qiskit 2.0.2 version 
+from qiskit.synthesis import OneQubitEulerDecomposer #(corrected to qiskit 2.0.2 version)
+from qiskit.synthesis import TwoQubitBasisDecomposer #(corrected to qiskit 2.0.2 version)
+from qiskit.circuit.library import CXGate
+
+from qiskit.quantum_info import partial_trace, DensityMatrix # (corrected to qiskit 2.0.2 version)
+
+#from pairwise_tomography.pairwise_state_tomography_circuits import pairwise_state_tomography_circuits
+#from pairwise_tomography.pairwise_fitter import PairwiseStateTomographyFitter
+#the above imports is dependent on qiskit-ignis and ignis is not supported in qiskit 2.x.x
+
+from qiskit_experiments.library.tomography import StateTomography # (corrected to qiskit-experiments 0.6.0 compatible with qiskit 2.0.2)
+from qiskit.result import Result # (corrected to qiskit 2.0.2 version)
+from qiskit_experiments.library.tomography import *
+
+from quantumgraph.ExpectationValue import ExpectationValue
 
 from numpy import pi, cos, sin, sqrt, exp, arccos, arctan2, conj, array, kron, dot, outer, nan, isnan
 import numpy as np
@@ -71,14 +90,14 @@ class QuantumGraph ():
         # use the `device` input to make a Qiskit backend object
         if type(device) is str:
             if device=='simulator':
-                self.backend = Aer.get_backend('qasm_simulator')
+                self.backend = AerSimulator() #You dont need to specify the backend as 'qasm_simulator', because in qiskit 2.x.x AerSimulator() is the default backend for qasm_simulation.
             else:
                 try:
                     if device[0:4]=='ibmq':
                         backend_name = device
                     else:
-                        backend_name = 'ibmq_' + backend
-                    for provider in IBMQ.providers():
+                        backend_name = 'ibmq_' + device # fixed to work with the IBMQ backend names
+                    for provider in IBMQ.providers(): 
                         for potential_backend in provider.backends():
                             if potential_backend.name()==backend_name:
                                 self.backend = potential_backend
@@ -122,13 +141,14 @@ class QuantumGraph ():
             Returns:
                 The job object of the submitted job.
             '''
-            if len(self.qc.data)==0:
-                job = execute(circs, Aer.get_backend('qasm_simulator'), shots=shots)
+            if len(self.qc.data) == 0:
+                sim = AerSimulator()
+                job = sim.run(circs, shots=shots)
             else:
                 submitted = False
-                while submitted==False:
+                while not submitted==False:
                     try:
-                        job = execute(circs, self.backend, shots=shots)
+                        job = self.backend.run(circs, shots=shots)
                         submitted = True
                     except:
                         print('Submission failed. Trying again in a minute')
@@ -145,25 +165,25 @@ class QuantumGraph ():
             Returns:
                 The results object for the circuits that have been run.
             '''
-            if self.backend.name()=='qasm_simulator' or len(self.qc.data)==0:
+            if self.backend.name() == 'qasm_simulator' or len(self.qc.data) == 0:
                 result = FakeResult()
+                sim = AerSimulator()
                 for circ in circs:
-                    counts = execute(circ, Aer.get_backend('qasm_simulator'), shots=shots).result().get_counts()
+                    counts = sim.run(circ, shots=shots).result().get_counts()
                     result.set_counts(circ,counts)
                 return result
             else:
                 job = submit_job(circs)
                 if job.backend().name()!='qasm_simulator':
                     time.sleep(1)
-                    while get_status(job)!='job has successfully run':
-                        m = 0
-                        while m<60 and get_status(job)!='job has successfully run':
-                            time.sleep(60)
-                            print(get_status(job))
-                            m += 1
-                        if get_status(job)!='job has successfully run':
-                            print('After 1 hour, job status is ' + get_status(job) + '. Another job will be submitted')
-                            job = submit_job(circs)
+                    m = 0
+                    while job.status().value != 'job has successfully run' and m < 60:
+                        time.sleep(60)
+                        print(get_status(job))
+                        m += 1
+                    if get_status(job)!='job has successfully run':
+                        print('After 1 hour, job status is ' + job.status().value + '. Another job will be submitted')
+                        job = submit_job(circs)
                 return job.result()
 
         if type(self.backend)==ExpectationValue:
@@ -172,60 +192,77 @@ class QuantumGraph ():
                                             pairs=self.backend.pairs)
             self.backend.apply_circuit(self.qc)
         else:
-            tomo_circs = pairwise_state_tomography_circuits(self.qc, self.qc.qregs[0])
-            tomo_results = get_result(tomo_circs)
-            self.tomography = PairwiseStateTomographyFitter(tomo_results, tomo_circs, self.qc.qregs[0])
-        
+            tomo_exp = StateTomography(self.qc)
+            exp_data = tomo_exp.run(self.backend, shots=shots)
+            exp_data.block_for_results()
+            self.tomography = exp_data.analysis_results(0).value  # This is the density matrix
     
     def get_bloch(self,qubit):
         '''
         Returns the X, Y and Z expectation values for the given qubit.
         '''
-        
+
         expect = {}
-        if type(self.backend)==ExpectationValue:
-            full_pauli = ['I']*self.num_qubits
+        if type(self.backend) == ExpectationValue:
+            full_pauli = ['I'] * self.num_qubits
             for pauli in ['X', 'Y', 'Z']:
                 full_pauli[qubit] = pauli
                 expect[pauli] = self.backend.pauli_decomp[''.join(full_pauli)]
         else:
-            if qubit==self.num_qubits-1:
-                q0,q1 = qubit-1,qubit
-            else:
-                q0,q1 = qubit,qubit+1
-            pair_expect = self.tomography.fit(output='expectation',pairs_list=[(q0,q1)])[q0,q1]
+            # --- REPLACE EVERYTHING BELOW THIS LINE ---
+            #if qubit==self.num_qubits-1:
+            #    q0,q1 = qubit-1,qubit
+            #else:
+            rho = self.tomography  # This is the full density matrix
+            # Build the single-qubit Pauli operator for the target qubit
             for pauli in ['X', 'Y', 'Z']:
-                pauli_pair = (pauli,'I')
-                if q0!=qubit:
-                    pauli_pair = tuple(list((pauli,'I'))[::-1])
-                expect[pauli] = pair_expect[pauli_pair]
-                    
+                op = 1
+                for i in range(self.num_qubits):
+                    if i == qubit:
+                        op = np.kron(op, matrices[pauli])
+                    else:
+                        op = np.kron(op, matrices['I'])
+                expect[pauli] = np.trace(rho @ op).real
         return expect
     
-    def get_relationship(self,qubit0,qubit1):  
+    def get_relationship(self, qubit0, qubit1):
         '''
         Returns the two qubit pauli expectation values for a given pair of qubits.
         '''
         relationship = {}
-        if type(self.backend)==ExpectationValue:
+        if type(self.backend) == ExpectationValue:
             full_pauli = ['I']*self.num_qubits
             for pauli in ['XX','XY','XZ','YX','YY','YZ','ZX','ZY','ZZ']:
                 full_pauli[qubit0] = pauli[0]
                 full_pauli[qubit1] = pauli[1]
-                relationship[pauli] = self.backend.pauli_decomp[''.join(full_pauli)]
-        else:
-            (q0,q1) = sorted([qubit0,qubit1])
-            reverse = (q0,q1)!=(qubit0,qubit1)
-            pair_expect = self.tomography.fit(output='expectation',pairs_list=[(q0,q1)])[q0,q1]
-            relationship = {}
-            for pauli in ['XX','XY','XZ','YX','YY','YZ','ZX','ZY','ZZ']:
-                if reverse:
-                    new_pauli = pauli[::-1]
+                key = ''.join(full_pauli)
+                if key in self.backend.pauli_decomp:
+                    val = self.backend.pauli_decomp[key]
                 else:
-                    new_pauli = pauli
-                relationship[new_pauli] = pair_expect[pauli[0],pauli[1]]
+                    # Use product of marginals if not present
+                    bloch0 = self.get_bloch(qubit0)
+                    bloch1 = self.get_bloch(qubit1)
+                    val = bloch0[pauli[0]] * bloch1[pauli[1]]
+                relationship[pauli] = val
+                # Reset for next loop
+                full_pauli[qubit0] = 'I'
+                full_pauli[qubit1] = 'I'
+        else:
+            rho = self.tomography  # Full density matrix
+            n = self.num_qubits
+            for pauli in ['XX','XY','XZ','YX','YY','YZ','ZX','ZY','ZZ']:
+                op = 1
+                for i in range(n):
+                    if i == qubit0:
+                        op = np.kron(op, matrices[pauli[0]])
+                    elif i == qubit1:
+                        op = np.kron(op, matrices[pauli[1]])
+                    else:
+                        op = np.kron(op, matrices['I'])
+                relationship[pauli] = np.trace(rho @ op).real
         return relationship
-    
+
+
     def set_bloch(self,target_expect,qubit,fraction=1,update=True):
         '''
         Rotates the given qubit towards the given target state.
@@ -275,11 +312,20 @@ class QuantumGraph ():
             state1 = [conj(state0[1]),-conj(state0[0])]
             
             return [state0,state1]
+        
+        # Debug: Print the current and target Bloch vectors
+        print(f"set_bloch called for qubit {qubit} with target_expect: {target_expect}, fraction: {fraction}, update: {update}")
 
         # add in missing zeros
         for pauli in ['X', 'Y', 'Z']:
             if pauli not in target_expect:
                 target_expect[pauli] = 0
+
+        # Debug: Print the current and target Bloch vectors
+        print(f"Current Bloch vector for qubit {qubit}: {self.get_bloch(qubit)}")
+
+        # normalize the target expectation values
+        print(f"Target Bloch vector for qubit {qubit}: {target_expect}")
         
         # determine the unitary which rotates as close to the target state as possible
         current_basis = get_basis(self.get_bloch(qubit))
@@ -290,16 +336,28 @@ class QuantumGraph ():
                 for k in range(2):
                     U[j][k] += target_basis[i][j]*conj(current_basis[i][k])
 
+        # Debug: Print the computed unitary matrix
+        print(f"Computed unitary matrix U for qubit {qubit}:\n{U}")
+
         # get the unitary for the desired fraction of the rotation
         if fraction!=1:
             U = pwr(U, fraction)
-
+        
+        # Debug: Print the fractional unitary matrix
+        print(f"Fractional unitary matrix U for qubit {qubit} (fraction={fraction}):\n{U}")
+        
         # apply the corresponding gate
         the,phi,lam = OneQubitEulerDecomposer().angles(U)
-        self.qc.u3(the,phi,lam,qubit)
+
+        # Debug: Print the Euler angles
+        print(f"Euler angles for qubit {qubit}: theta={the}, phi={phi}, lambda={lam}")
+
+        self.qc.u(the, phi, lam, qubit)
 
         if update:
             self.update_tomography()
+            # Debug: Print confirmation of tomography update
+            print("Tomography updated after applying set_bloch.")
              
     def set_relationship(self,relationships,qubit0,qubit1,fraction=1, update=True):
         '''
@@ -346,8 +404,11 @@ class QuantumGraph ():
                     rho += rel[pauli]*matrices[pauli]
                 return rho/4
             else:
-                (q0,q1) = sorted([qubit0,qubit1])                                     
-                return self.tomography.fit(output='density_matrix',pairs_list=[(q0,q1)])[q0,q1]
+                # Extract the reduced density matrix for qubit0 and qubit1
+                rho_full = DensityMatrix(self.tomography)
+                reduced = partial_trace(rho_full, [i for i in range(self.num_qubits) if i not in [qubit0, qubit1]])
+                # The result is a 2-qubit DensityMatrix object
+                return reduced.data
 
         raw_vals,raw_vecs = la.eigh( get_rho(qubit0,qubit1) )
         vals = sorted([(val,k) for k,val in enumerate(raw_vals)], reverse=True)
@@ -396,7 +457,17 @@ class QuantumGraph ():
             valid[3] = True not in [isnan(new_vecs[3][j]) for j in range(4)]
             
         # a unitary is then constructed to rotate the old basis into the new
-        U = [[0 for _ in range(4)] for _ in range(4)]
+        '''
+        The unitary is constructed as follows:
+        U = sum_j ( |new_vecs[j]><vecs[j]| )
+        
+        where |new_vecs[j]> is the j-th new vector and <vecs[j]| is the j-th old vector.
+        If the fraction is not 1, then the unitary is raised to the power of `fraction`.
+        The unitary is then decomposed into a circuit using the CX gate.
+        The circuit is then appended to the quantum circuit.
+        If the decomposition fails, None is returned.
+        '''
+        U = np.zeros((4, 4), dtype=complex) # What changed here is the initialization of U to a complex array because the outer product will produce complex numbers.
         for j in range(4):
             U += outer(new_vecs[j],conj(vecs[j]))
 
@@ -404,9 +475,10 @@ class QuantumGraph ():
             U = pwr(U, fraction)
             
         try:
-            circuit = two_qubit_cnot_decompose(U)
+            decomposer = TwoQubitBasisDecomposer(CXGate())
+            circuit = decomposer(U)
             gate = circuit.to_instruction()
-            done = True
+            #done = True
         except Exception as e:
             print(e)
             gate = None
