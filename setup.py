a81cdc131940b4b3e76b19de09ee0a8187d0cc3f
quantumjim <decodoku@gmail.com>
2025-06-06 16:26:32 +0200

    add aer

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='quantumgraph',
-      install_requires=['qiskit', 'scipy', 'qiskit-experiments'],
+      install_requires=['qiskit', 'scipy', 'qiskit-experiments', 'qiskit-aer'],
       version='0.0.1',
       packages=['quantumgraph']
 )
